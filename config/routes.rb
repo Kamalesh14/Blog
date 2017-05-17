81d3d59db87380b5f05be455f3a8955488c363c8
Kamalesh Maurya <kamaleshmaurya@Kamalesh-Maurya.local>
2017-05-17 15:58:23 +0530

    added new view file

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
 
   get 'helloworld/show'
 
-  get 'welcome/index'
+  get 'helloworld/greet'
 
    resources :articles do
   resources :comments
 end
 
-  root 'welcome#index'
+  root controller: 'helloworld', action: 'index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
